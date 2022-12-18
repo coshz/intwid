4e9613b8b2760418202c98e412fc66fad2e8ba40
coshz <fsinhx@gmail.com>
2022-12-18 14:49:49 +0800

    remove 3rd-party deps

--- trash/Package.swift
+++ trash/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+//        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,7 +23,7 @@ let package = Package(
         .target(
             name: "Trash",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
+//                .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]),
         .testTarget(
             name: "trashTests",
--- trash/README.md
+++ trash/README.md
@@ -2,7 +2,17 @@
 
 A commandline tool to move files/directorys to the trash.
 
-usage:
+## usage
+
 1. `trash -h` for help;
 2. eg. `trash a1.md aa/*.swift bb/`;
+
+
+## update
+
+version 0.1.1, 2022.11.10
+1. Remove argument-parser dependency;
+
+version 0.1.0
+1. A commandline tool to move files/directorys to the trash.
 
